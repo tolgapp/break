9e88ba4061d0471c25fe2b9c45159fdf0d8de2df
Tolga <123948207+tolgapp@users.noreply.github.com>
2025-07-25 11:49:04 +0200

    style improvements @ product detail

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -44,11 +44,9 @@ const ProductDetail: React.FC<ProductDetail> = ({
   };
 
   return (
-    <div
-      className={`product-container fixed inset-0 z-[100] ${openDetail ? 'fade-in' : 'fade-out'}`}
-    >
+    <div className={`fixed inset-0 z-[100] ${openDetail ? 'fade-in' : 'fade-out'}`}>
       <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-xl"></div>
-      <div className="fixed flex flex-col justify-center items-center bottom-0 z-[200] h-[75%] w-full bg-slate-50 rounded-t-3xl rounded-xl">
+      <div className="fixed flex flex-col justify-center items-center bottom-0 z-[200] h-[73%] w-full bg-slate-50 rounded-t-3xl rounded-xl">
         <button
           onClick={() => closeDetail(null)}
           className="absolute top-4 right-4 w-12 h-12 flex justify-center items-center border border-white text-white text-3xl rounded-full bg-black z-50 hover:bg-red-700"
@@ -57,8 +55,10 @@ const ProductDetail: React.FC<ProductDetail> = ({
         </button>
         <div className="w-full px-8">
           <img
-            className={`top-0 rounded-t-3xl translate-y-[-20rem] absolute left-0 ${
-              selectedCoffee.name === 'Iced Coffee' ? 'translate-y-[-19rem]' : ''
+            className={`top-0 absolute left-0 ${
+              selectedCoffee.name === 'Iced Coffee'
+                ? 'translate-y-[-21rem]'
+                : ' translate-y-[-18rem]'
             }`}
             src={selectedCoffee.image}
             alt="coffee mockup cup"
@@ -79,7 +79,7 @@ const ProductDetail: React.FC<ProductDetail> = ({
           </div>
           <div className="w-full mt-10">
             <h3 className="text-3xl font-semibold mb-4">Sizes and Prices:</h3>
-            <div className="flex gap-4 flex-wrap">
+            <div className="flex gap-fit justify-between flex-wrap">
               {selectedCoffee.sizes.map((size, index) => {
                 const isSelected = selectedSize === size;
                 return (
@@ -99,20 +99,22 @@ const ProductDetail: React.FC<ProductDetail> = ({
             </div>
           </div>
         </div>
-        <button
-          onClick={handleAddToCart}
-          className="mt-8 bg-black text-white text-2xl px-6 py-3 rounded-lg hover:bg-orange-800 hover:text-white"
-          aria-label="Add to cart"
-        >
-          Add to Cart
-        </button>
-        <Link
-          to="/cart"
-          onClick={() => closeDetail(null)}
-          className="mt-4 bg-black text-white text-2xl px-6 py-3 rounded-lg hover:bg-green-700 hover:text-white"
-        >
-          Go to Checkout
-        </Link>
+        <div className="flex items-center space-x-8 mt-14">
+          <button
+            onClick={handleAddToCart}
+            className="bg-black text-white text-2xl px-6 py-3 rounded-lg hover:bg-orange-800 hover:text-white"
+            aria-label="Add to cart"
+          >
+            Add to Cart
+          </button>
+          <Link
+            to="/cart"
+            onClick={() => closeDetail(null)}
+            className="bg-black text-white text-2xl px-6 py-3 rounded-lg hover:bg-green-700 hover:text-white"
+          >
+            Go to Checkout
+          </Link>
+        </div>
       </div>
     </div>
   );
